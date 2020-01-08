583ab48cc65c24bb0f5111efb1e2319313128b4b
DFSWeb <contato@dfsweb.com.br>
2020-01-08 09:56:11 -0300

    Criação de endpoints e documentação do sistema

--- routers.php
+++ routers.php
@@ -2,3 +2,19 @@
 global $routes;
 $routes = array();
 
+//Usuarios
+$routes['/users/login'] = '/users/login';
+$routes['/users/new'] = '/users/new_record';
+$routes['/users/{id}'] = '/users/view/:id';
+$routes['/users/{id}/feed'] = '/users/feed/:id';
+$routes['/users/{id}/photos'] = '/users/photos/:id';
+$routes['/users/{id}/follow'] = '/users/follow/:id';
+
+//Fotos
+$routes['/photos/random'] = '/photos/random';
+$routes['/photos/new'] = '/photos/new_record';
+$routes['/photos/{id}'] = '/photos/view/:id';
+$routes['/photos/{id}/comment'] = '/photos/comment/:id';
+$routes['/photos/{id}/like'] = '/photos/like/:id';
+
+//Endpoints criadas
