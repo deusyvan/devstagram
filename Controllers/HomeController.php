385789d0a15fc5f6bfa36fb6e48c44388036412c
root <root@dfsweb.com.br>
2020-01-08 00:17:56 -0300

    Zerando a estrutura

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -7,18 +7,6 @@ use Models\Usuarios;
 class HomeController extends Controller{
     //
     public function index(){
-        $array = array(
-            'nome' => 'Antonio',
-            'idade' => '10'
-        );
-        $this->returnJson($array);
-    }
-    
-    public function testando() {
-        echo "FUNCIONOU";
-    }
-    
-    public function visualizar_usuarios($id) {
-        echo "ID: ".$id;
+        
     }
 }
--- routers.php
+++ routers.php
@@ -2,5 +2,3 @@
 global $routes;
 $routes = array();
 
-$routes['/teste'] = '/home/testando';
-$routes['/usuarios/{id}'] = '/home/visualizar_usuarios/:id';
